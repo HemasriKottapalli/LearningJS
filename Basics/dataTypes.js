1e802fdeb62e7de41520a809d44e7902a033a1a0
HemasriKottapalli <kottapallihemasri@gmail.com>
2023-12-30 12:19:52 +0530

    datatypes

---
+++ Basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+
+//primitive data types in js
+//number
+//bigInt
+//string
+//boolean
+//null undefined
+
+console.log(typeof "Hema"); //typeof
+
+//type of null is object
+//type of undefined is undefined
+
+let x = null;
+let y ;
+
+console.log(typeof x);
+console.log(typeof y);
